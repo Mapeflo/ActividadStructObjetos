0d726fb465898d0fc6dad7405aa971fb7278b376
mapeflo <mpenarandaf@unicartagena.edu.co>
2026-09-12 23:27:57 -0500

    feat(python): recorrido de arreglo de structs Estudiante

--- python/3_struct_recorrido.py
+++ python/3_struct_recorrido.py
@@ -6,14 +6,13 @@ class Estudiante:
     edad: int
     promedio: float
 
-# Crear las 3 instancias
 estudiante1 = Estudiante("María López", 20, 4.5)
 estudiante2 = Estudiante("Carlos Ramírez", 22, 3.8)
 estudiante3 = Estudiante("Ana Gómez", 19, 4.2)
 
 estudiantes = [estudiante1, estudiante2, estudiante3]
 
-print("===== LISTA DE ESTUDIANTES =====")
+print("LISTA DE ESTUDIANTES:")
 print()
 
 for i, est in enumerate(estudiantes, start=1):
